22750906941dec994ac483fbb7c625ee4b864b67
Peter Tasner <peter.tasner@conceptualise.de>
2023-04-21 14:41:32 +0200

    footer added

--- src/lotto-app/src/components/Footer.js
+++ src/lotto-app/src/components/Footer.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 mt-16 fixed bottom-0 w-full">
       <div className="container mx-auto text-center">
+        <div className="flex justify-center items-center space-x-4 mb-4">
+          <a
+            href="https://github.com/your-github-username"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-400 transition-colors duration-200"
+          >
+            <FontAwesomeIcon icon={faGithub} size="lg" />
+          </a>
+          <a
+            href="https://twitter.com/your-twitter-handle"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-400 transition-colors duration-200"
+          >
+            <FontAwesomeIcon icon={faTwitter} size="lg" />
+          </a>
+        </div>
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} Lucky Shiba. All rights reserved.
+          &copy; {new Date().getFullYear()} Your Company. All rights reserved.
         </p>
       </div>
     </footer>
